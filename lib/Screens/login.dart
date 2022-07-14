d6d9239d254de8be22668617eeb252486211d7a9
Abdul Manaf <manaf@cordovacloud.com>
2022-07-14 15:39:21 +0530

    qwer

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -11,10 +12,169 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: 100,
-        height: 100,
-        color: Colors.red,
+      backgroundColor: Color(0xff831EE5),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(top: 0.25.sh),
+          width: 1.sw,
+          height: 0.75.sh,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(25), topLeft: Radius.circular(25)),
+              color: Colors.white),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 0.05.sh,
+              ),
+              Text(
+                'Please login with registered email id',
+                style: TextStyle(fontSize: 12.sp,fontFamily: 'Axiforma',color: Color(0xff8A8CBD),fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: 0.03.sh,
+              ),
+              Container(
+                width: 278,
+                height: 65,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(color: Color(0xff518EF8))),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 0.2.sw,
+                    ),
+                    Image(image: AssetImage('assets/images/Googleicon.png')),
+                    SizedBox(
+                      width: 0.05.sw,
+                    ),
+                    Text('Google',style: TextStyle(fontFamily: 'Axiforma',color: Color(0xff007AB9),fontWeight: FontWeight.w500,fontSize: 22.0871.sp),)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 0.02.sh,
+              ),
+              Text('or',style: TextStyle(fontSize: 12.sp,fontFamily: 'Axiforma',fontWeight: FontWeight.w400,color: Color(0xff8A8CBD)),),
+              SizedBox(
+                height: 0.02.sh,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 50, right: 50),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.people,
+                      color: Color(0xffD2C2FF),
+                    ),
+                    helperText: ' ',
+                    labelText: 'Email or Mobile Number',
+                    labelStyle: TextStyle(color: Color(0xFFD2C2FF)),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFFD2C2FF)),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF7D22E1)),
+                    ),
+                  ),
+                  style: TextStyle(fontFamily: 'Axiforma',fontWeight: FontWeight.w400,fontSize: 12.sp),
+                ),
+              ),
+              SizedBox(
+                height: 0.001.sh,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 50, right: 50),
+                child: TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.lock_open_outlined,
+                      color: Color(0xffD2C2FF),
+                    ),
+                    suffixIcon: Icon(
+                      Icons.remove_red_eye_outlined,
+                      color: Color(0xffD2C2FF),
+                    ),
+                    helperText: ' ',
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Color(0xFFD2C2FF)),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFFD2C2FF)),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF7D22E1)),
+                    ),
+                  ),
+                    style: TextStyle(fontFamily: 'Axiforma',fontWeight: FontWeight.w400,fontSize: 12.sp)
+                ),
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 0.12.sw,
+                  ),
+                  Icon(Icons.lock_open, color: Color(0xff8A8CBD)),
+                  SizedBox(
+                    width: 0.02.sw,
+                  ),
+                  Text(
+                    'Forgot Password',
+                    style: TextStyle(fontSize: 10.sp, color: Color(0xff8A8CBD),fontWeight: FontWeight.w400,fontFamily: 'Montserrat'),
+                  ),
+                  SizedBox(
+                    width: 0.27.sw,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(fontSize: 16.sp,fontFamily: 'Montserrat',fontWeight: FontWeight.w400),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Color(0xff25DBDB),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6))),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 0.08.sh,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 0.12.sw,
+                  ),
+                  Image(image:AssetImage('assets/images/Benchmarklogo.png'),height: 40,),
+                  SizedBox(
+                    width: 0.1.sw,
+                  ),
+                  Image(image:AssetImage('assets/images/GooglePic.png'),height: 40,),
+                ],
+              ),
+              SizedBox(
+                height: 0.04.sh,
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 0.06.sh,
+                  ),
+                  Text('Need any help?',style: TextStyle(fontSize: 11.sp,fontFamily: 'Axiforma',color: Color(0xff8A8CBD),fontWeight: FontWeight.w400),),
+                  SizedBox(
+                    width: 0.01.sh,
+                  ),
+                  Text('Contact:',style: TextStyle(fontSize: 11.sp,fontFamily: 'Axiforma',color: Color(0xff8A8CBD),fontWeight: FontWeight.w400)),
+                  SizedBox(
+                    width: 0.003.sh,
+                  ),
+                  Text('support@team-sqa.com',style: TextStyle(fontSize: 11.sp,fontFamily: 'Axiforma',color: Color(0xff8A8CBD),fontWeight: FontWeight.w400))
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
