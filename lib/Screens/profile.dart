5abdebee46a980e6f5f5baa1b5f757463dbf5c0a
Abdul Manaf P Fassal <manaf@mmgc-india.org>
2022-07-15 17:20:05 +0530

    homepagedone

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:school_diary2022/Widgets/bottomNavBar.dart';
 import '../Widgets/sideBar.dart';
 import '../Widgets/customAppbar.dart';
@@ -19,10 +20,205 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _key,
-      appBar: CustomAppBar(name: true,title: 'Home',appHeight: 200,centreText: false,globalKey: _key),
-      drawer: SideBar(),
-      bottomNavigationBar: BottomNavBar(),
-    );
+        key: _key,
+        // appBar: CustomAppBar(name: true,title: 'Home',appHeight: 200,centreText: false,globalKey: _key),
+        drawer: SideBar(),
+        bottomNavigationBar: BottomNavBar(),
+        body: Container(
+          width: 1.sw,
+          height: 1.sh,
+          child: Stack(
+            children: [
+              CustomAppBar(
+                  name: true,
+                  title: 'Home',
+                  appHeight: 120,
+                  centreText: false,
+                  globalKey: _key),
+              Align(
+                  alignment: Alignment(0, -0.65),
+                  child: nameCard(
+                      grade: 'Grade 12B',
+                      photourl:
+                          'https://images.unsplash.com/photo-1554126807-6b10f6f6692a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+                      studentName: 'Christopher Howard')),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  width: 1.sw,
+                  height: 1.sh / 2 + 60,
+                  child: ListView(
+                    children: [listdItem(), listdItem(), listdItem()],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
+
+  Widget nameCard(
+          {required String studentName,
+          required String photourl,
+          required String grade}) =>
+      Container(
+        width: 1.sw - 40,
+        height: 150,
+        padding: EdgeInsets.symmetric(vertical: 15),
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              blurRadius: 5,
+              offset: Offset(1, 1),
+            )
+          ],
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 35,
+              backgroundColor: Color(0xff8829e1),
+              child: CircleAvatar(
+                radius: 32,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(photourl),
+                ),
+              ),
+            ),
+            Text(
+              studentName,
+              style: TextStyle(
+                  color: Color(0xff8829e1),
+                  fontFamily: 'Axiforma',
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w500),
+            ),
+            Text(
+              grade,
+              style: TextStyle(
+                  color: Color(0xffcd758e),
+                  fontFamily: 'Axiforma',
+                  fontSize: 10.sp,
+                  fontWeight: FontWeight.w500),
+            )
+          ],
+        ),
+      );
+  Widget listdItem() => Container(
+        width: 1.sw - 40,
+        height: 260,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black54,
+              blurRadius: 5,
+              offset: Offset(1, 1),
+            )
+          ],
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 120,
+              height: 30,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              //color: Colors.red,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color(0x19cd758e),
+                  border: Border.all(color: Color(0xfffd5186))),
+              child: Row(
+                children: [
+                  Icon(Icons.campaign_outlined),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Circular',
+                    style: TextStyle(
+                        color: Color(0xfffd5586),
+                        fontSize: 13.sp,
+                        fontFamily: 'Axiforma'),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                    width: 250,
+                    height: 50,
+                    child: Text(
+                      'sint vel voluptatem sint vel voluptatem',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: Color(0xff3e3e3e),
+                        fontSize: 16.sp,
+                        fontFamily: 'Axiforma',
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),)
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              width: 1.sw,
+              height: 60,
+              child: Text('Voluptas vero consequuntur molestias et. Dolores fuga voluptatem. Optio laboriosam ut amet assumenda perferendis.',style: TextStyle(
+                color: Color(0xff787878),
+                fontSize: 12.sp,
+                fontFamily: 'Axiforma',
+              ),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('Updated On',style: TextStyle(
+                  color: Color(0xff787878),
+                  fontSize: 10.sp,
+                  fontFamily: 'Axiforma',
+                  fontStyle: FontStyle.italic
+                ),),
+                SizedBox(width: 10,),
+                Icon(Icons.calendar_month_outlined),
+                SizedBox(width: 10,),
+                Text('date',style: TextStyle(
+                  color: Color(0xff252525),
+                  fontSize: 11.sp,
+                  fontFamily: 'Axiforma',
+                ),)
+              ],
+            )
+          ],
+        ),
+      );
 }
--- lib/Widgets/bottomNavBar.dart
+++ lib/Widgets/bottomNavBar.dart
@@ -1,87 +1,169 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({Key? key}) : super(key: key);
 
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int? _selctedIndex;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 1.sw,
-      height: 0.1.sh,
-      padding: EdgeInsets.symmetric(horizontal: 6,vertical: 2),
+      height: 0.12.sh,
+      //padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(25),topRight: Radius.circular(25),),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black,
-            blurRadius: 5,
-            offset: Offset(1, 1)
-          )
-        ]
-      ),
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+          ),
+          boxShadow: [
+            BoxShadow(color: Colors.black, blurRadius: 5, offset: Offset(1, 1))
+          ]),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           bottomMenuItem(
             menuIcon: Icons.campaign_outlined,
             menuName: 'Circular',
             onTapColor: Color(0xfffd5386),
+            indexNo: 0,
+          ),
+          bottomMenuItem(
+            menuIcon: Icons.menu_book_outlined,
+            menuName: 'Assignment',
+            onTapColor: Color(0xff4966ff),
+            indexNo: 1,
+          ),
+          bottomMenuItem(
+            menuIcon: Icons.calendar_month_outlined,
+            menuName: 'Calendar',
+            onTapColor: Color(0xffbb49ff),
+            indexNo: 2,
+          ),
+          bottomMenuItem(
+            menuIcon: Icons.payments_outlined,
+            menuName: 'Fee',
+            onTapColor: Color(0xff00b59c),
+            indexNo: 3,
+          ),
+          bottomMenuItem(
+            menuIcon: Icons.pie_chart,
+            menuName: 'Assessments',
+            onTapColor: Color(0xfffd5386),
+            indexNo: 4,
           )
         ],
       ),
     );
   }
-}
 
-class bottomMenuItem extends StatefulWidget {
-  final IconData menuIcon;
-  final String menuName;
-  final Color onTapColor;
-  const bottomMenuItem({
-    Key? key,
-    required this.menuIcon,
-    required this.menuName,
-    required this.onTapColor
-    
-  }) : super(key: key);
-
-  @override
-  State<bottomMenuItem> createState() => _bottomMenuItemState();
-}
-
-class _bottomMenuItemState extends State<bottomMenuItem> {
-  bool isClicked = false;
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: (){
-        setState((){
-          isClicked = true;
-        });
-      },
-      child: Container(
-        padding: EdgeInsets.all(4),
-        width: 60,
-        height: 70,
-        //color: Colors.red,
-        child: Column(
-          children: [
-            CircleAvatar(
-              radius: 20,
-              backgroundColor: isClicked ? widget.onTapColor : Color(0xfff2f2f2),
-              child: Icon(widget.menuIcon),
+  Widget bottomMenuItem({required int indexNo,required IconData menuIcon,required String menuName,required Color onTapColor}) => InkWell(
+    onTap: () {
+      setState(() {
+       // isClicked = true;
+        _selctedIndex = indexNo;
+      });
+    },
+    child: Container(
+      //padding: EdgeInsets.all(4),
+      width: 1.sw / 5,
+      height: 70,
+      //color: Colors.red,
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            backgroundColor:
+            _selctedIndex == indexNo ? onTapColor : Color(0xfff2f2f2),
+            child: Icon(
+              menuIcon,
+              color: _selctedIndex == indexNo ? Colors.white :Color(0xff818181),
             ),
-            SizedBox(height: 2,),
-            Text(widget.menuName,style: TextStyle(
-              fontSize: 11.sp,
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          Text(
+            menuName,
+            style: TextStyle(
+              fontSize: 10.sp,
               fontFamily: 'Axiforma',
               fontWeight: FontWeight.w400,
-              color: isClicked ? widget.onTapColor : Color(0xff787878),
-            ),)
-          ],
-        ),
+              color: _selctedIndex == indexNo ? onTapColor : Color(0xff787878),
+            ),
+          )
+        ],
       ),
-    );
-  }
+    ),
+  );
 }
+
+// class bottomMenuItem extends StatefulWidget {
+//   final int indexNo;
+//   final IconData menuIcon;
+//   final String menuName;
+//   final Color onTapColor;
+//   const bottomMenuItem(
+//       {Key? key,
+//       required this.menuIcon,
+//       required this.menuName,
+//       required this.onTapColor,
+//       required this.indexNo})
+//       : super(key: key);
+//
+//   @override
+//   State<bottomMenuItem> createState() => _bottomMenuItemState();
+// }
+//
+// class _bottomMenuItemState extends State<bottomMenuItem> {
+//  // int? _selctedIndex;
+//   bool isClicked = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: () {
+//         setState(() {
+//           isClicked = true;
+//           _selctedIndex = widget.indexNo;
+//         });
+//       },
+//       child: Container(
+//         //padding: EdgeInsets.all(4),
+//         width: 1.sw / 5,
+//         height: 70,
+//         //color: Colors.red,
+//         child: Column(
+//           children: [
+//             CircleAvatar(
+//               radius: 20,
+//               backgroundColor:
+//                   _selctedIndex == widget.indexNo ? widget.onTapColor : Color(0xfff2f2f2),
+//               child: Icon(
+//                 widget.menuIcon,
+//                 color: Color(0xff818181),
+//               ),
+//             ),
+//             SizedBox(
+//               height: 2,
+//             ),
+//             Text(
+//               widget.menuName,
+//               style: TextStyle(
+//                 fontSize: 10.sp,
+//                 fontFamily: 'Axiforma',
+//                 fontWeight: FontWeight.w400,
+//                 color: _selctedIndex == widget.indexNo ? widget.onTapColor : Color(0xff787878),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
